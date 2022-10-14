cf2c59826eaf4cfac3d5e3382a072f88cac4da2f
Cullen Rafferty <cullen1882@gmail.com>
2022-10-14 18:28:56 -0400

    boot created

---
+++ CommercialRealEstate/src/main/java/com/skilldistillery/jpacommercialrealestate/CommercialRealEstateApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacommercialrealestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialRealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialRealEstateApplication.class, args);
+	}
+
+}
---
+++ CommercialRealEstate/src/test/java/com/skilldistillery/jpacommercialrealestate/CommercialRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacommercialrealestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialRealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
